928c92bea434634857dd7a3e218c0a3f5d52020c
Jason Spidle <jason.spidle@gmail.com>
2025-02-14 18:12:14 -0600

    initial keymap edits and rule definitions

---
+++ keyboards/keebio/iris_lm/keymaps/panoptican/keymap.c
@@ -0,0 +1,79 @@
+#include QMK_KEYBOARD_H
+
+// Tap dance settings
+#define TAPPING_TERM 180
+#define QUICK_TAP_TERM 0
+
+// Auto shift settings
+#define AUTO_SHIFT_TIMEOUT 200	
+#define NO_AUTO_SHIFT_ALPHA
+
+// Left-hand aliases
+#define LCTL_BS LCTL_T(KC_BSPC)
+#define LCTL_S LCTL_T(KC_S)
+#define LALT_D LALT_T(KC_D)
+#define LGUI_F LGUI_T(KC_F)
+#define HYPR_G ALL_T(KC_G)
+#define MEH_B MEH_T(KC_B)
+#define GUI_SPC LGUI_T(KC_SPACE)
+#define ALT_RET LALT_T(KC_ENTER)
+
+// Right-hand aliases
+#define HYPR_H ALL_T(KC_H)
+#define RGUI_J RGUI_T(KC_J)
+#define RALT_K RALT_T(KC_K)
+#define RCTL_L RCTL_T(KC_L)
+#define MEH_N MEH_T(KC_N)
+
+
+
+enum custom_layers {
+     _QWERTY,
+     _LOWER,
+     _RAISE
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+      QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      LCTL_BS, KC_A,    LCTL_S   LALT_D,  LGUI_F,  HYPR_G,                             HYPR_H,  RGUI_J,  RALT_K,  RCTL_L,  KC_SCLN, KC_QUOT,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    MEH_B,   KC_SPC,           KC_SPC,  MEH_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                     GUI_SPC, ALT_RET, KC_ENT,                    KC_SPC,  KC_SPC,  KC_SPC
+                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_LOWER] = LAYOUT(
+   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      KC_GRV,  _______, KC_UP,   _______, QK_BOOT, RESET,                              0x7c03,  KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PGDN,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_PIPE,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      RM_NEXT, EE_CLR,  _______, _______, _______, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
+   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                     KC_VOLU, KC_VOLD, KC_MUTE,                   KC_DEL,  KC_BRID, KC_BRIU
+                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_RAISE] = LAYOUT(
+   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      RM_TOGG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, QK_BOOT,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      RM_NEXT, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_HOME, RM_HUEU, RM_SATU, RM_VALU, KC_BSLS,
+   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+      KC_MUTE, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  RM_HUED, RM_SATD, RM_VALD, EE_CLR,
+   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                     _______, _______, _______,                   _______, _______, _______
+                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  )
+};
