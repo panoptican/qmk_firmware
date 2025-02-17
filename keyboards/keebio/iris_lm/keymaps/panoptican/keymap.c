9bf0b1a021e9e802006b91ac6c20af7483dca540
Jason Spidle <jason.spidle@gmail.com>
2025-02-16 21:08:55 -0600

    Add minus, equal, alfred

--- keyboards/keebio/iris_lm/keymaps/panoptican/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/panoptican/keymap.c
@@ -9,6 +9,9 @@
 #define MEH_B MEH_T(KC_B)
 #define GUI_SPC LGUI_T(KC_SPACE)
 #define ALT_RET LALT_T(KC_ENTER)
+#define LT_RET LT(1,KC_ENTER)
+#define ALF LGUI(KC_SPACE)
+
 
 // Right-hand aliases
 #define HYPR_H ALL_T(KC_H)
@@ -16,6 +19,7 @@
 #define RALT_K RALT_T(KC_K)
 #define RCTL_L RCTL_T(KC_L)
 #define MEH_N MEH_T(KC_N)
+#define LT_SPC LT(1,KC_SPACE)
 
 enum custom_layers {
      _QWERTY,
@@ -28,18 +32,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-      QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+      QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL,
    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
    //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
       LCTL_BS, KC_A,    LCTL_S,  LALT_D,  LGUI_F,  HYPR_G,                             HYPR_H,  RGUI_J,  RALT_K,  RCTL_L,  KC_SCLN, KC_QUOT,
    //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    MEH_B,   KC_HOME,          KC_END,  MEH_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
    //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     GUI_SPC, ALT_RET, KC_ENT,                    KC_SPC,  KC_SPC,  KC_SPC
+                                     GUI_SPC, ALT_RET, LT_RET,                    LT_SPC,  KC_SPC,  KC_SPC
                                  // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
    ),
- 
+
    [_LOWER] = LAYOUT(
    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
@@ -53,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      KC_VOLU, KC_VOLD, KC_MUTE,                   KC_DEL,  KC_BRID, KC_BRIU
                                  // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
    ),
- 
+
    [_RAISE] = LAYOUT(
    //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
       KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
